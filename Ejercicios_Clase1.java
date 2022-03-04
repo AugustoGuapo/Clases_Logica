e5a917a01678da3c98fdeb47f4094972a0cdb46f
AugustoGuapo <86898468+AugustoGuapo@users.noreply.github.com>
2022-03-04 11:47:15 -0500

    Add files via upload

---
+++ Ejercicios_Clase1.java
@@ -0,0 +1,46 @@
+package ejercicios_clase1;
+import java.util.Scanner;
+public class Ejercicios_Clase1 {
+
+    public static void main(String[] args) {
+        /* EJERCICIO 1
+        Scanner entrada = new Scanner(System.in);
+        int dato1;
+        int dato2;
+        System.out.println("Digite la primera cantidad");
+        dato1 = entrada.nextInt();
+        System.out.println("Digite la segunda cantidad");
+        dato2 = entrada.nextInt();
+        
+        int suma = dato1+dato2;
+        int resta = dato1-dato2;
+        int multiplicacion = dato1*dato2;
+        int division = dato1/dato2;
+        
+        System.out.println("Su resultados son: Suma = " + suma + " Resta = " + resta + " Multiplicación = " + multiplicacion + " Division = " + division);
+        */
+        
+        /* EJERCICIO 2
+        Scanner entrada = new Scanner(System.in);
+        String nombre;
+        String apellido;
+        String cedula;
+        String fechaDeNacimiento;
+        System.out.println("Digite su nombre");
+        nombre = entrada.nextLine();
+        System.out.println("Digite su apellido");
+        apellido = entrada.nextLine();
+        System.out.println("Digite su cedula");
+        cedula = entrada.nextLine();
+        System.out.println("Digite su fecha de nacimiento");
+        fechaDeNacimiento = entrada.nextLine();
+        
+        System.out.println(nombre);
+        System.out.println(apellido);
+        System.out.println(cedula);
+        System.out.println(fechaDeNacimiento);
+        */
+        
+    }
+    
+}
